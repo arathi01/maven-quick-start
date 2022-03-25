b3779865b7825039ec5e8d2e5b56068801d53100
Arathi Krishnan <arathikrishnan949@gmail.com>
2022-03-25 15:43:56 +0000

    added "thank you"in the application"

--- src/main/java/ait/course/example/Application.java
+++ src/main/java/ait/course/example/Application.java
@@ -28,6 +28,8 @@ public static void main(String[] args){
         app.greet();
         int count = app.countWords("I have four words");
         System.out.println("Word Count: "+count);
+        System.out.println("Thank You...");
+
    
 }
 
